691fce06a886b1b58f36caa0023032a72fed0623
Eni Hoxha <46694067+heniox@users.noreply.github.com>
2021-11-08 17:55:20 +0100

    first commit

---
+++ Folders.cs
@@ -0,0 +1,192 @@
+﻿using Newtonsoft.Json;
+using Serilog;
+using System;
+
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+namespace VGICancellazioneFile
+{
+    class Folders
+    {
+        public string Path { get; set; }
+        public int NumDays { get; set; }
+        public string DatiVerificare { get; set; } = "Creation Date";
+        public bool SubFolders { get; set; } = false;
+        public string Estensione { get; set; } 
+
+        [JsonIgnore]
+        public int DeletedFiles { get; set; } = 0;
+        public delegate int DeleteFiles(DirectoryInfo dir);
+        public void DeleteSubFolders(string path, DeleteFiles deleteFiles)
+        {
+            try
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(path);
+                Log.Information(String.Format("Searching subfolders: {0}", path));
+                //Remove the files
+                int deletedFiles = deleteFiles(di);
+                DeletedFiles += deletedFiles;
+                Log.Information(String.Format("Removed {0} files from subfolders: {1}", deletedFiles, path));
+                Parallel.ForEach(di.EnumerateDirectories(), dir =>
+                {
+                    DeleteSubFolders(dir.FullName, deleteFiles);
+                });
+             }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error:" + e.Message);
+                Log.Error(e, e.Message);
+            }
+        }
+
+        public void DeleteDirectory()
+        {
+            try
+            {               
+                Log.Information(String.Format("Searching folders {0} for files with extension  {1} and {2} older than {3}", this.Path, this.Estensione, this.DatiVerificare, this.NumDays));
+                //check if directory exist
+                if (Directory.Exists(this.Path))
+                {
+                    DirectoryInfo di = new DirectoryInfo(this.Path);
+                    //check if we are physically removing files from folders or just testing application              
+                    DeleteFiles deleteFiles =null;
+                    if (Program.TestMode)
+                    {
+                        deleteFiles = DeleteFilesTesting;
+                    }
+                    else
+                    {
+                        deleteFiles = DeleteFilesPysically;
+
+                    }
+                    //removing files from directory
+                    DeletedFiles += deleteFiles(di);
+                    //check if the app shpuld also search subfolders
+                    if (this.SubFolders)
+                    {
+                        Log.Information("Searching files in subfolder:{0}",this.Path);
+
+                        Parallel.ForEach(di.EnumerateDirectories(), dir =>
+                        {
+                            DeleteSubFolders(dir.FullName, deleteFiles);
+                        });
+
+                    }
+                    Log.Information(String.Format("Removed {0} from folder: {1}", DeletedFiles, this.Path));
+                }
+                else
+                {
+                    Log.Warning(String.Format("Could not find this directory: {0}", this.Path));
+                    Console.WriteLine(String.Format("Could not find this directory: {0}", this.Path));
+               
+                }
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+        public int DeleteFilesPysically(DirectoryInfo dir)
+        {
+            try
+            {
+                 int deletedFiles = 0;
+                ParallelOptions parallelOptions = new ParallelOptions();
+                parallelOptions.MaxDegreeOfParallelism = Environment.ProcessorCount * 2;
+                //schedule work across multiple threads based on your system environment
+                Parallel.ForEach(dir.EnumerateFiles()
+                    .Where(f => Delete(this, f)), parallelOptions, file =>
+                    {
+
+                        try
+                        {
+                            Log.Information(String.Format("Removed file: {0}  with creation date:{1} and modificcation date: {2}", file.FullName,file.CreationTime,file.LastWriteTime));
+                            //eleminare il file                           
+                                    file.Delete();   
+                        }
+                        catch (Exception e)
+                        {
+                            //Console.WriteLine("Error:" + e.Message);
+                            Log.Error(e, e.Message);
+                        }
+                        deletedFiles++;
+                    }
+
+              );
+
+                //return the number of deleted files
+                return deletedFiles;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+        public int DeleteFilesTesting(DirectoryInfo dir)
+        {
+            try
+            {
+                int deletedFiles = 0;
+                ParallelOptions parallelOptions = new ParallelOptions();
+                parallelOptions.MaxDegreeOfParallelism = Environment.ProcessorCount * 2;
+                //schedule work across multiple threads based on your system environment
+                Parallel.ForEach(dir.EnumerateFiles()
+                    .Where(f => Delete(this, f)), parallelOptions, file =>
+                    {
+
+                        try
+                        {
+                         
+                            Log.Information(String.Format("Eliminare il file: {0}  la DATA DI CREAZIONE:{1} e la DATA DI MODIFICA: {2}", file.FullName, file.CreationTime, file.LastWriteTime));
+                         
+                        }
+                        catch (Exception e)
+                        {
+                            //Console.WriteLine("Error:" + e.Message);
+                            Log.Error(e, e.Message);
+                        }
+                        deletedFiles++;
+                    }
+
+              );
+
+                //return the number of deleted files
+                return deletedFiles;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        public Func<Folders, FileInfo, bool> Delete = delegate (Folders fileInstance, FileInfo f) {
+            bool shouldDelete = false;
+            //check if the calculation should be done on creation date or modification date.
+            if (fileInstance.DatiVerificare.Replace(" ", "").ToLower().Equals("modificationdate"))
+            {
+                shouldDelete = (DateTime.Now - f.LastWriteTime)
+                .TotalDays >= fileInstance.NumDays;
+            }
+            else
+            {
+                shouldDelete = (DateTime.Now - f.CreationTime)
+               .TotalDays >= fileInstance.NumDays;
+            }
+            //check the file extension
+            if (!fileInstance.Estensione.Equals(".*"))
+                shouldDelete = shouldDelete && f.Extension.Equals(fileInstance.Estensione);
+            return shouldDelete;
+        };
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VGICancellazioneFile
+{
+    class Program
+    {
+        public static IConfiguration config;
+        public static bool TestMode { get; set; }
+        static void Main(string[] args) 
+        {
+            //configuration of the app launch
+            AppStart();
+            Console.WriteLine("Start delete");
+            Log.Information("Start delete");
+            //begin the job
+            StartDeleting();
+          
+        }
+
+        public static void StartDeleting()
+        {
+            try
+            {
+                Log.Information("Read the files from the external configuration file");
+                //Read the files from the external configuration file
+                List<Folders> cartellaList = config.GetSection("FilesConfiguration").Get<List<Folders>>();
+                TestMode =config.GetValue<bool>("TestMode");
+                Log.Information(String.Format("Found {0} folders", cartellaList.Count));
+                //schedule work across multiple threads based on your system environment
+                Parallel.ForEach(cartellaList, cartella =>                {
+                    Console.WriteLine(String.Format("Started working with file {0} thread {1}", cartella.Path, Thread.CurrentThread.ManagedThreadId));
+                    cartella.DeleteDirectory();
+                });
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, e.Message);
+                Console.WriteLine("Error:" + e.Message);
+            }
+
+        }
+
+
+
+        public static void AppStart()
+        {
+
+            Console.WriteLine("Start the application");
+            // Code that runs on application startup
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("Configuration.json")
+              .AddEnvironmentVariables();
+
+            config = builder.Build();
+            ServiceCollection serviceCollection = new ServiceCollection();
+            // Specifying the configuration for serilog
+            Log.Logger = new LoggerConfiguration() // initiate the logger configuration
+                            .ReadFrom.Configuration(builder.Build()) // connect serilog to our configuration folder
+                            .Enrich.FromLogContext() //Adds more information to our logs from built in Serilog 
+                            .WriteTo.File(String.Concat(config["Serilog:File"], DateTime.Now.ToString("yyyyMMddhhmmss"), ".txt")) // decide where the logs are going to be shown
+                            .CreateLogger(); //initialise the logger
+
+            serviceCollection.AddSingleton(LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddSerilog(dispose: true);
+            }));
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+
+        }
+
+    }
+}
